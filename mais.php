21eb500542e8451d65bc9ce39fb09be3bd9b54f0
Michael Edwards <smededwards@gmail.com>
2025-03-17 10:15:20 +0000

    Initial commit

---
+++ mais.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       Mais
+ * Plugin URI:        https://github.com/smededwards/mais/
+ * Author:            Michael Edwards
+ * Author URI:        https://github.com/smededwards/
+ * Description:
+ * Version:           0.1.0
+ * Requires at least: 6.7
+ * Requires PHP:      7.4
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       mais
+ *
+ * @package CreateBlock
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
